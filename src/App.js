71e685245b7517c355d5eb6f13a82a8b475d3411
PatrykPańczuk <p.panczuk1@gmail.com>
2023-07-01 01:08:10 +0200

    dodanie nawigacji i wstepny wyglad

--- src/App.js
+++ src/App.js
@@ -1,18 +1,35 @@
 import './App.css';
 import React, { useEffect, useState } from 'react';
+import { Button } from '@mui/material';
+import { BrowserRouter, Routes, Route, Link, useParams } from 'react-router-dom';
 
 function App() {
+    return (
+        <BrowserRouter>
+            <div className="App">
+                <header className="App-header">
+                    <p>Aplikacja pogodowa</p>
+                </header>
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/:day" element={<DetailedForecast />} />
+                </Routes>
+                <footer className="App-footer">
+                    <p>Created by <b>Patryk Pańczuk</b></p>
+                </footer>
+            </div>
+        </BrowserRouter>
+    );
+}
+
+function Home() {
     const [pogodaKrakow, setPogodaKrakow] = useState(null);
-    const [pogodaJutro, setPogodaJutro] = useState(null);
-    const [pogodaPoJutrze, setPogodaPoJutrze] = useState(null);
 
     useEffect(() => {
         fetchWeatherData('Krakow', setPogodaKrakow);
-        fetchWeatherData('Krakow', setPogodaJutro, 1);
-        fetchWeatherData('Krakow', setPogodaPoJutrze, 2);
     }, []);
 
-    const fetchWeatherData = async (location, setData, daysOffset = 0) => {
+    const fetchWeatherData = async (location, setData) => {
         try {
             const response = await fetch(
                 `https://api.open-meteo.com/v1/forecast?latitude=50.06&longitude=19.94&hourly=temperature_2m&daily=temperature_2m_max,temperature_2m_min&current_weather=true&timezone=Europe%2FBerlin`
@@ -27,46 +44,56 @@ function App() {
         }
     };
 
+    return (
+        <div className={"content"}>
+                <h1>Pogoda w Krakowie</h1>
+                {pogodaKrakow && (
+                    <div>
+                        <h2 ><p id={"dzis"}>Pogoda na dziś </p> </h2>
+                        <p>Temperatura maksymalna: <b>{pogodaKrakow.daily.temperature_2m_max[0]}°C</b> </p>
+                        <p>Temperatura minimalna: <b>{pogodaKrakow.daily.temperature_2m_min[0]}°C</b> </p>
+                        <Button variant="contained" component={Link} to="/dzis">
+                            Szczegółowa prognoza
+                        </Button>
+                    </div>
+                )}
+                {pogodaKrakow && (
+                    <div>
+                        <h2 ><p id={"jutro"}>Pogoda na jutro </p> </h2>
+                        <p>Temperatura maksymalna: <b>{pogodaKrakow.daily.temperature_2m_max[1]}°C</b> </p>
+                        <p>Temperatura minimalna:<b> {pogodaKrakow.daily.temperature_2m_min[1]}°C</b></p>
+                        <Button variant="contained" component={Link} to="/jutro">
+                            Szczegółowa prognoza
+                        </Button>
+                    </div>
+                )}
+                {pogodaKrakow && (
+                    <div >
+                        <h2><p id={"pojutrze"}>Pogoda na pojutrze</p> </h2>
+                        <p>Temperatura maksymalna: <b>{pogodaKrakow.daily.temperature_2m_max[2]}°C</b> </p>
+                        <p>Temperatura minimalna: <b>{pogodaKrakow.daily.temperature_2m_min[2]}°C</b></p>
+                        <Button variant="contained" component={Link} to="/pojutrze">
+                            Szczegółowa prognoza
+                        </Button>
+                    </div>
+                )}
+        </div>
+    );
+}
 
+function DetailedForecast() {
+    const { day } = useParams();
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>Aplikacja pogodowa</p>
-      </header>
-      <body>
-      <div>
-          <h1>Pogoda w Krakowie</h1>
-          {/* Wyświetl informacje o pogodzie dla Krakowa, jutra i pojutrze */}
-          {pogodaKrakow && (
-              <div>
-                  <h2>Pogoda na dziś</h2>
-                  <p>Temperatura maksymalna: {pogodaKrakow.daily.temperature_2m_max[0]}°C</p>
-                  <p>Temperatura minimalna: {pogodaKrakow.daily.temperature_2m_min[0]}°C</p>
-                  {/* Wyświetl inne informacje pogodowe */}
-              </div>
-          )}
-          {pogodaJutro && (
-              <div>
-                  <h2>Pogoda na jutro</h2>
-                  <p>Temperatura maksymalna: {pogodaJutro.daily.temperature_2m_max[1]}°C</p>
-                  <p>Temperatura minimalna: {pogodaJutro.daily.temperature_2m_min[1]}°C</p>
-                  {/* Wyświetl inne informacje pogodowe */}
-              </div>
-          )}
-          {pogodaPoJutrze && (
-              <div>
-                  <h2>Pogoda na pojutrze</h2>
-                  <p>Temperatura maksymalna: {pogodaPoJutrze.daily.temperature_2m_max[2]}°C</p>
-                  <p>Temperatura minimalna: {pogodaPoJutrze.daily.temperature_2m_min[2]}°C</p>
-                  {/* Wyświetl inne informacje pogodowe */}
-              </div>
-          )}
-      </div>
-      </body>
-      <footer className="App-footer"><p>Created by<b> Patryk Pańczuk</b></p></footer>
-    </div>
-  );
+    // Tutaj uzupelnic
+
+    return (
+        <div className={"content"}>
+            <h1>Szczegółowa prognoza na {day}</h1>
+            <Button variant="contained" component={Link} to="/">
+                Powrót
+            </Button>
+        </div>
+    );
 }
 
 export default App;
